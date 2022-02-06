87b9da6e5bc30c07605859ce0f30aec35771e161
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-06 14:35:36 +0100

    first comment

---
+++ menu-server/src/main/java/com/menu/menuserver/MenuServerApplication.java
@@ -0,0 +1,13 @@
+package com.menu.menuserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MenuServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MenuServerApplication.class, args);
+	}
+
+}
