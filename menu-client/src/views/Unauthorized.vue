7eae0d2570cea4bfd8f1a0cf340140573b8f14c0
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-10 12:56:52 +0100

    feat(frontend): added unauthorized  page

--- menu-client/src/views/Unauthorized.vue
+++ menu-client/src/views/Unauthorized.vue
@@ -1,5 +1,17 @@
 <template>
-  <div>Unauthorized</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 text-center">
+        <h1 class="display-1 mt-20">401</h1>
+        <div class="display-6 mb-5">
+          Unauthorized! Access Permission Denied!
+        </div>
+        <router-link to="/home" class="btn btn-link"
+          >Back to Home Page</router-link
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +20,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
