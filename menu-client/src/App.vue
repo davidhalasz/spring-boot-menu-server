84f1ba2c672c76dd88610d87d1029a0c027e0b54
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-10 12:20:13 +0100

    added routes to views

--- menu-client/src/App.vue
+++ menu-client/src/App.vue
@@ -1,30 +1,31 @@
 <template>
-  <div id="nav">
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <div>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+      <div class="container">
+        <div class="navbar-nav me-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/home" active-class="active"> Home </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/profile" active-class="active"> Profile </router-link>
+          </li>
+        </div>
+
+        <div class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <router-link class="nav-link" to="/register" active-class="active"> Sign Up </router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="nav-link" to="/login" active-class="active"> Login </router-link>
+          </li>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container">
+      <router-view />
+    </div>
   </div>
-  <router-view />
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+<style></style>
--- menu-client/src/router/index.js
+++ menu-client/src/router/index.js
@@ -1,20 +1,49 @@
 import { createRouter, createWebHistory } from "vue-router";
 import Home from "../views/Home.vue";
+import Login from "../views/Login.vue";
+import Register from "../views/Register.vue";
+import Profile from "../views/Profile.vue";
+import NotFound from "../views/NotFound.vue";
+import Unauthorized from "../views/Unauthorized.vue";
 
 const routes = [
   {
     path: "/",
-    name: "Home",
     component: Home,
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+    path: "/home",
+    name: "home",
+    component: Home,
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login,
+  },
+  {
+    path: "/register",
+    name: "register",
+    component: Register,
+  },
+  {
+    path: "/profile",
+    name: "profile",
+    component: Profile,
+  },
+  {
+    path: "/404",
+    name: "404",
+    component: NotFound,
+  },
+  {
+    path: "/401",
+    name: "401",
+    component: Unauthorized,
+  },
+  {
+    path: "/:catchAll(.*)",
+    redirect: "/404",
   },
 ];
 
