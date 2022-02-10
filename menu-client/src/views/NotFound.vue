963802a5a3ddc7ba45212382e8c670a2e6326722
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-10 12:53:09 +0100

    feat(frontend): added not found page

--- menu-client/src/views/NotFound.vue
+++ menu-client/src/views/NotFound.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>Not Found</div>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-12 col-lg-8, col-xl-6 text-center">
+        <h1 class="display-1 mt-20">404</h1>
+        <div class="display-6 mb-5">Opps! Page not found!</div>
+        <router-link to="/home" class="btn btn-link"
+          >Back to Home Page</router-link
+        >
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -8,6 +18,4 @@ export default {
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
