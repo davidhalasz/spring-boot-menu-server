89c27ea40b538ff0c073d0713984a427a4cb21ff
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-11 15:21:07 +0100

    feat(frontend): added menu table has been implemented

--- menu-client/src/views/Profile.vue
+++ menu-client/src/views/Profile.vue
@@ -1,13 +1,55 @@
 <template>
-  <div>Profile</div>
+  <div class="container">
+    <div class="mt-5">
+      <div class="card">
+        <div class="card-header">
+          <div class="row">
+            <div class="col-6">
+              <h3>Added Menu</h3>
+            </div>
+          </div>
+        </div>
+
+        <div class="card-body">
+          <table class="table">
+            <thead>
+              <tr>
+                <th scope="col">username</th>
+                <th scope="col">description</th>
+                <th scope="col">action</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>menud</td>
+                <td>menudesc</td>
+                <td><button class="btn btn-primary">Edit</button></td>
+                <td><button class="btn btn-danger">Delete</button></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import MenuService from "../services/menu.service";
+import Menu from "../models/menu";
 export default {
   name: "profile",
+  data() {
+    return {
+      menu: new Menu(),
+    };
+  },
+  mounted() {
+    MenuService.displayMenu().then(response => {
+      this.menu = response.data;
+    });
+  }
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
