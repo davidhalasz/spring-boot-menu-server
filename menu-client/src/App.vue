331759e47f6845eb6148df6b3c60fb4c48e653e8
davidhalasz <38536685+davidhalasz@users.noreply.github.com>
2022-02-23 17:23:34 +0100

    feat(frontend): added styles to home page

--- menu-client/src/App.vue
+++ menu-client/src/App.vue
@@ -63,4 +63,11 @@ export default {
 }
 </script>
 
-<style></style>
+<style>
+@import url("https://fonts.googleapis.com/css2?family=VT323&display=swap");
+
+.navbar {
+  font-family: "VT323", monospace;
+  font-size: 25px;
+}
+</style>
--- menu-client/src/views/Home.vue
+++ menu-client/src/views/Home.vue
@@ -1,5 +1,24 @@
 <template>
-  <div>Home</div>
+  <div class="row home">
+    <div class="col-6 home-text h-100 row align-items-center">
+      <div>
+        <p class="home-title">Create Your Own Menu</p>
+        <p class="home-content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus
+          auctor ornare lacus sed rutrum. Duis ut nisi metus. Aenean a
+          sollicitudin ante, quis tempor urna. Fusce quis consectetur tellus.
+        </p>
+      </div>
+    </div>
+    <div class="col-6 images">
+      <div class="img1">
+        <img src="../assets/home_1.png" />
+      </div>
+      <div class="img2">
+        <img src="../assets/home-qr.png" />
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -9,5 +28,54 @@ export default {
 </script>
 
 <style scoped>
+@import url("https://fonts.googleapis.com/css2?family=VT323&display=swap");
 
+.home {
+  height: 100vh;
+}
+.home-text {
+  font-family: "VT323", monospace;
+}
+.home-title {
+  font-size: 50px;
+}
+.home-content {
+  font-size: 20px;
+}
+.images {
+  width: 350px;
+  height: 500px;
+  position: relative;
+  margin-left: 70px;
+}
+.img1 {
+  height: 100%;
+  width: auto;
+  -ms-transform: skewY(-8deg); /* IE 9 */
+  transform: skewY(-8deg);
+  position: absolute;
+  top: 0;
+  left: 0;
+  margin-top: 50px;
+}
+.img1 img {
+  height: 100%;
+  border: 5px black solid;
+}
+.img2 {
+  height: 50%;
+  width: auto;
+  -ms-transform: skewY(-8deg); /* IE 9 */
+  transform: skewY(-8deg);
+  position: absolute;
+  z-index: 2;
+  top: 0;
+  right: 0;
+  margin-top: 150px;
+}
+
+.img2 img {
+  height: 100%;
+  border: 5px black solid;
+}
 </style>
